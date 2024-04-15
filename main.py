0e53a81302a1a99dd88848e1525f1ff203d8ec84
Antony Bailey <antony.bailey@thepoliceoftheinter.net>
2024-04-15 16:34:12 +0100

    Trunk

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from socket_factory import SocketFactory
 from message_processor import MessageProcessor
+from socket_factory import SocketFactory
+
 
 def main():
     # Prompt the user for host and port
-    host = input("Enter host (default: 127.0.0.1): ") or '127.0.0.1'
+    host = input("Enter host (default: 127.0.0.1): ") or "127.0.0.1"
     port = input("Enter port (default: 2237): ") or 2237
     port = int(port)  # Convert port to integer
 
@@ -12,6 +13,7 @@ def main():
 
     message_processor = MessageProcessor(socket)
     message_processor.process_messages()
+
 
 if __name__ == "__main__":
     main()
